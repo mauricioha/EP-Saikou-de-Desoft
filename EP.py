ecbe5366982db8b520e3c2974f79131c37f88c9d
FernandoESanches <38226394+FernandoESanches@users.noreply.github.com>
2018-04-18 14:36:47 -0300

    Fernando
    acho que melhor deixar acao ao invés de produto.

---
+++ EP.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 18 13:44:18 2018
+
+@author: FERNANDO
+"""
+produtos={}
+acao=int(input('''Controle de estoque
+            0 - sair
+            1 - adicionar item
+            2 - remover item
+            3 - alterar item
+            4 - imprimir estoque
+            Faça sua escolha: '''))
+
+while acao!=0:
+    if acao == 1:
+        produto = input('Qual o produto?')
+        if produto in produtos:
+                print('Produto ja está cadastrado')
+        else:
+            quantidade = int(input('Quantidade?'))    
+            while quantidade < 0:
+                print('Só é aceito numero positivo')
+                quantidade = int(input('Quantidade?'))
+            produtos[produto] = quantidade
+    acao=int(input('''Controle de estoque
+            0 - sair
+            1 - adicionar item
+            2 - remover item
+            3 - alterar item
+            4 - imprimir estoque
+            Faça sua escolha: '''))
+    elif acao == 2:
+        produto = input('Qual o produto?')
+            
+print(produtos)
