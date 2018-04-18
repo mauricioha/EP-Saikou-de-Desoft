ab83737118a842cc0fe79b085d0f606120b386dc
mauricioha <38228116+mauricioha@users.noreply.github.com>
2018-04-18 15:55:17 -0300

    Maurício
    Terminei o item 1. Modifiquei os nomes e implementei o json.

--- EP1.py
+++ EP1.py
@@ -1,4 +1,5 @@
-escolha=int(input(''' 
+import json
+acao=int(input(''' 
 Controle do estoque
 0 - sair
 1 - adicionar item
@@ -6,13 +7,41 @@ Controle do estoque
 3 - alterar item
 4 - imprimir estoque
 Faça sua escolha: '''))
-produtos=dict()
-if escolha==0:
-    print("Até mais!")
-elif escolha==1:
-    nome=input('Nome do produto: ')
-    quantidade=int(input('Quantidade inicial'))
-    while quantidade<0:
-        print('A quantidade inicial não pode ser negativa.')
-        quantidade=int(input('Quantidade inicial: '))
-        
+estoque=dict()
+while acao!=0:
+    if acao==1:
+        produto=input('Nome do produto: ')
+        if produto not in estoque:
+            quantidade=int(input('Quantidade inicial: '))
+            while quantidade<0:
+                print('A quantidade inicial não pode ser negativa.')
+                quantidade=int(input('Quantidade inicial: '))
+            estoque[produto]={'quantidade':quantidade}
+        else:
+            print('Produto já cadastrado.')
+    elif acao==2:
+        produto=input('Nome do produto: ')
+        if produto not in estoque:
+            print('Elemento não encontrado.')
+        else:
+            del estoque[produto]
+    elif acao==3:
+        produto=input('Nome do produto: ')
+        if produto not in estoque:
+            print('Elemento não encontrado.')
+        else:
+            quantidade=int(input('Quantidade: '))
+            estoque[produto]['quantidade']+=quantidade
+    elif acao==4:
+        for produto in estoque:
+            print('{0} : {1}'.format(produto, estoque[produto]['quantidade']))
+    acao=int(input(''' 
+Controle do estoque
+0 - sair
+1 - adicionar item
+2 - remover item
+3 - alterar item
+4 - imprimir estoque
+Faça sua escolha: '''))
+dicionario=json.dumps(estoque, sort_keys=True, indent=4)
+print("Até mais!")
